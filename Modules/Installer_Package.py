0660c94298040692e35dd7d986e584f8496a8342
Leo Pitt <17372992+D00MFist@users.noreply.github.com>
2021-07-08 14:43:03 -0400

    async update

--- Modules/Installer_Package.py
+++ Modules/Installer_Package.py
@@ -79,7 +79,7 @@ def install_pkg():
         await scripting()
         try:
             while True:
-                pending = asyncio.Task.all_tasks()
+                pending = asyncio.all_tasks()
                 plist = []
                 for p in pending:
                     if p._coro.__name__ != "main" and p._state == "PENDING":
@@ -89,7 +89,7 @@ def install_pkg():
                 else:
                     await asyncio.gather(*plist)
         except KeyboardInterrupt:
-            pending = asyncio.Task.all_tasks()
+            pending = asyncio.all_tasks()
             for t in pending:
                 t.cancel()    
 
