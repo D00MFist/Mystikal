d4a6915bd0ef7fd7cb6b804568e4cceeefd068bb
Leo Pitt <17372992+D00MFist@users.noreply.github.com>
2021-07-08 14:46:40 -0400

    Update Macro_Word.py

--- Modules/Macro_Word.py
+++ Modules/Macro_Word.py
@@ -85,7 +85,7 @@ def macro_word():
         await scripting()
         try:
             while True:
-                pending = asyncio.Task.all_tasks()
+                pending = asyncio.all_tasks()
                 plist = []
                 for p in pending:
                     if p._coro.__name__ != "main" and p._state == "PENDING":
@@ -95,7 +95,7 @@ def macro_word():
                 else:
                     await asyncio.gather(*plist)
         except KeyboardInterrupt:
-            pending = asyncio.Task.all_tasks()
+            pending = asyncio.all_tasks()
             for t in pending:
                 t.cancel()    
 
