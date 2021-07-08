437109aa0dfccc9e09908c48761462f13f85ed6e
Leo Pitt <17372992+D00MFist@users.noreply.github.com>
2021-07-08 14:47:26 -0400

    Update PDF.py

--- Modules/PDF.py
+++ Modules/PDF.py
@@ -85,7 +85,7 @@ def pdf():
         await scripting()
         try:
             while True:
-                pending = asyncio.Task.all_tasks()
+                pending = asyncio.all_tasks()
                 plist = []
                 for p in pending:
                     if p._coro.__name__ != "main" and p._state == "PENDING":
@@ -95,7 +95,7 @@ def pdf():
                 else:
                     await asyncio.gather(*plist)
         except KeyboardInterrupt:
-            pending = asyncio.Task.all_tasks()
+            pending = asyncio.all_tasks()
             for t in pending:
                 t.cancel()    
 
