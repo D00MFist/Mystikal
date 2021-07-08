0d3b3b229ba3d9e390a492fa644125df7b2dc2df
Leo Pitt <17372992+D00MFist@users.noreply.github.com>
2021-07-08 14:45:23 -0400

    Update Installer_Plugins.py

--- Modules/Installer_Plugins.py
+++ Modules/Installer_Plugins.py
@@ -76,7 +76,7 @@ def install_plug():
         await scripting()
         try:
             while True:
-                pending = asyncio.Task.all_tasks()
+                pending = asyncio.all_tasks()
                 plist = []
                 for p in pending:
                     if p._coro.__name__ != "main" and p._state == "PENDING":
@@ -86,7 +86,7 @@ def install_plug():
                 else:
                     await asyncio.gather(*plist)
         except KeyboardInterrupt:
-            pending = asyncio.Task.all_tasks()
+            pending = asyncio.all_tasks()
             for t in pending:
                 t.cancel()    
 
