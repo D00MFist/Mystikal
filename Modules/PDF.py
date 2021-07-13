8476963d0696aefa88729145c72b974bd5e5a63a
Leo Pitt <17372992+D00MFist@users.noreply.github.com>
2021-07-13 15:05:02 -0400

    Update PDF.py

--- Modules/PDF.py
+++ Modules/PDF.py
@@ -57,7 +57,7 @@ def pdf():
         resp = await mythic.create_payload(p, all_commands=True, wait_for_build=True)
 
         print("[*] Building PDF Payload")
-        payloadDownloadid = resp.response.file_id.agent_file_id
+        payloadDownloadid = resp.response.file["agent_file_id"]
 
         url = "https://" + mythic_server_ip + ":" + mythic_server_port + "/api/v1.4/files/download/" + payloadDownloadid # modify to point to desired location
 
