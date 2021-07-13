d3698a3ce04c8dc7466c5bbc4aeffd59a6839d32
Leo Pitt <17372992+D00MFist@users.noreply.github.com>
2021-07-13 15:03:16 -0400

    Update Macro_Excel.py

--- Modules/Macro_Excel.py
+++ Modules/Macro_Excel.py
@@ -48,7 +48,7 @@ def macro_excel():
         # if we define commands in the payload definition, then remove the all_commands=True piece
         resp = await mythic.create_payload(p, all_commands=True, wait_for_build=True)
 
-        payloadDownloadid = resp.response.file_id.agent_file_id #.response.id
+        payloadDownloadid = resp.response.file["agent_file_id"]
         url = "https://" + mythic_server_ip + ":" + mythic_server_port + "/api/v1.4/files/download/" + payloadDownloadid # modify to point to desired location
 
         #Download Payload 
