aae4a838d2f56dfb37507f428ee99baa9a8ff50a
Leo Pitt <17372992+D00MFist@users.noreply.github.com>
2021-07-29 10:11:55 -0400

    Delete AppIDFull.sh

--- Templates/MobileConfigChrome/AppIDFull.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-openssl rsa -pubout -outform DER < /Users/student/Desktop/mystikal-D00MFist-dev/Payloads/MobileConfigChrome_Payload/temp/extension.pem | shasum -a 256 | head -c32 | tr 0-9a-f a-p > /Users/student/Desktop/mystikal-D00MFist-dev/Payloads/MobileConfigChrome_Payload/temp/AppIDValue
